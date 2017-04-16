648a8e9656429b2c5b06a9a506563104d55939e6
vrushali254 <vmahajan@bu.edu>
2017-04-16 01:50:49 -0400

    Parser.js can now parse lines

--- parser.js
+++ parser.js
@@ -1,3 +1,14 @@
-function parser{
-  
+function main()
+{
+    parser();
 }
+
+function parser()
+{
+    var txtBox = document.getElementById("textbox");
+    var lines  =  txtBox.value.split("\n");
+    document.getElementById("demo").innerHTML = lines;
+}
+// print out last line to page
+
+
